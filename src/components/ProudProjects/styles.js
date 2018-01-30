e8197237840c7c01bc82704ae926fbfa4e7ab25c
Yifei Wang <hello.yifei@icloud.com>
2018-01-30 23:13:17 +0100

    yarn build for gh-pages

--- src/components/ProudProjects/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  color: white;
-  border-left: 5px solid rgba(255, 255, 255, 0.5);
-  padding-left: 70px;
-  margin-left: 20px;
-  position:relative;
-`;
